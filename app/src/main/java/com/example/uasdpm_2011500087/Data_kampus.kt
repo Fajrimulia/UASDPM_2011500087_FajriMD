843dad5dcb92b9c675b601dbdb803349bf4e762b
Fajrimulia <fajri.mulia.darma@gmail.com>
2022-06-21 09:48:10 -0800

    Initial commit

---
+++ app/src/main/java/com/example/uasdpm_2011500087/Data_kampus.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm_2011500087
+
+class Data_kampus (
+    var nidn: String,
+    var nmDosen: String,
+    var jabatan: String,
+    var golonganpangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var programstudi: String,
+)
